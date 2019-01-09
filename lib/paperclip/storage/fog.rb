59b4b5d843558d3beb1d73b581e86696129e61ac
Selva Muthu Kumar <selva@voonik.com>
2019-01-09 13:33:38 +0530

    Using Google Fog Storage - Compatibity Fix

--- lib/paperclip/storage/fog.rb
+++ lib/paperclip/storage/fog.rb
@@ -44,7 +44,7 @@ module Paperclip
 
         base.instance_eval do
           unless @options[:url].to_s.match(/^:fog.*url$/)
-            @options[:path]  = @options[:path].gsub(/:url/, @options[:url]).gsub(/^:rails_root\/public\/system\//, '')
+            @options[:path]  = @options[:path].call(self).gsub(/:url/, @options[:url]).gsub(/^:rails_root\/public\/system\//, '')
             @options[:url]   = ':fog_public_url'
           end
           Paperclip.interpolates(:fog_public_url) do |attachment, style|
@@ -138,15 +138,27 @@ module Paperclip
         end
       end
 
-      def expiring_url(time = (Time.now + 3600), style = default_style)
-        if directory.files.respond_to?(:get_http_url)
-          expiring_url = directory.files.get_http_url(path(style), time)
+      def scheme
+        @scheme ||= fog_credentials[:scheme] || 'https'
+      end
+      def convert_time(time)
+        if time.is_a?(Integer)
+          time = Time.now + time
+        end
+        time
+      end
+
+      def expiring_url(time = (Time.now + 3600), style_name = default_style)
+        time = convert_time(time)
+        http_url_method = "get_#{scheme}_url"
+        if path(style_name) && directory.files.respond_to?(http_url_method)
+          expiring_url = directory.files.public_send(http_url_method, path(style_name), time)
 
           if @options[:fog_host]
-            expiring_url.gsub!(/#{host_name_for_directory}/, dynamic_fog_host_for_style(style))
+            expiring_url.gsub!(/#{host_name_for_directory}/, dynamic_fog_host_for_style(style_name))
           end
         else
-          expiring_url = public_url
+          expiring_url = url(style_name)
         end
 
         return expiring_url
